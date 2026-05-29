6087d8a950e8191e00692a8cbe15439f6830c37f
Lukan Vanderlinde <github@lukan.rocks>
2026-05-29 16:20:12 -0300

    sonner

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -6,7 +6,7 @@ import { createRoot } from 'react-dom/client'
 import { Theme } from '@/theme'
 import { AppRoutes } from '@/routes'
 import { SettingsProvider } from '@/contexts/settings-context'
-import { Toaster } from '@/components/feedback/sonner'
+import { Toaster } from '@/components/4-templates/sonner'
 
 const App = () => (
   <StrictMode>
